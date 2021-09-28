a5ed0fadb52bea78e00461088bef64bda5190628
MarianMaschkeRGST <m.maschke@rgst.net>
2021-09-28 13:52:54 +0900

    added image banner gallery

--- private_projects/img_banner.js
+++ private_projects/img_banner.js
@@ -3,29 +3,36 @@ function listener(e) {
     // let y;
     // To do: Still bugging but find way to make z-index back to 1 on mouseleave
     if(e.type == "mouseenter")
-        x = 2.5;
+        x = 0.5;
         // y = 10;
     else if (e.type == "mouseleave")
-        x = 1;
+        x = 0;
         // y = 1;
     anime({targets: e.target, 
-            scale: x, 
+            scale: x+1, 
             zIndex:{
-                value: 10,
+                value: x*10,
                 round: true,
-            }});
+            },
+            duration: 200,
+            easing: "easeOutCirc",
+            borderColor: "#ffffff",
+            borderWidth: x*6,
+        });
     }
 
-// Create Grid 9*16 
+// Create Grid 4*6 
 // i is rows, j is columns
+// Adjust css variables --gallery-img-wno and --gallery-img-hno. accordingly.
 function loadPage(){
     let counter = 0;
-    for(let i = 0; i < 9; i++)
-        for(let j = 0; j < 16; j++) {
+    for(let i = 0; i < 4; i++)
+        for(let j = 0; j < 6; j++) {
             counter++;
             let div = document.createElement('div');
             div.classList.add(`image-banner`)
-            div.classList.add(`img-${counter}`)
+            // div.classList.add(`img-${counter}`)
+            div.setAttribute("style", `background-image: url(./img/img-${counter}.jpeg);`)
             document.getElementById("banner").appendChild(div);
         }
     
