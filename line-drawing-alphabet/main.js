fff103ca35d52bd4cf7eb7c3460799b3a2aa3ff9
MarianMaschkeRGST <m.maschke@rgst.net>
2021-10-01 18:20:55 +0900

    added line drawing animations

--- line-drawing-alphabet/main.js
+++ line-drawing-alphabet/main.js
@@ -1,9 +1,11 @@
-var lineDrawing = anime({
+function onLoad(){
+  anime({
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 3000,
+    duration: 8000,
     delay: function(el, i) { return i * 250 },
     direction: 'alternate',
     loop: true
   });
+}
